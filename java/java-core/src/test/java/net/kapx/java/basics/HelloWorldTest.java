067d390287c3ac1085ea685c7321cce776f4e538
KAPIL <xkapil@gmail.com>
2015-11-13 11:38:35 +0000

    new module added

--- java/java-core/src/test/java/net/kapx/java/basics/HelloWorldTest.java
+++ java/java-core/src/test/java/net/kapx/java/basics/HelloWorldTest.java
@@ -1,25 +1,37 @@
 package net.kapx.java.basics;
 
+import junit.framework.Assert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class HelloWorldTest {
-	private HelloWorld helloWorld;
+    private HelloWorld helloWorld;
 
-	@Before
-	public void setUp() throws Exception {
-		helloWorld = new HelloWorld();
-	}
+    @Before
+    public void setUp() throws Exception {
+        helloWorld = new HelloWorld();
+    }
 
-	@After
-	public void tearDown() throws Exception {
-		helloWorld = null;
-	}
+    @After
+    public void tearDown() throws Exception {
+        helloWorld = null;
+    }
 
-	@Test(expected=IllegalArgumentException.class)
-	public void sayHello_withNullArgument_throwsIllegalArgumentException() {
-		helloWorld.sayHello(null);
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void sayHello_withNullArgument_throwsIllegalArgumentException() {
+        helloWorld.sayHello(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void sayHello_withEmptyArgument_throwsIllegalArgumentException() {
+        helloWorld.sayHello("");
+    }
+
+    @Test
+    public void sayHello_withValidArgument_returnCorrectResults() {
+        final String actual = helloWorld.sayHello("Kapx");
+        Assert.assertEquals("Hello ! Kapx", actual);
+    }
 
 }
