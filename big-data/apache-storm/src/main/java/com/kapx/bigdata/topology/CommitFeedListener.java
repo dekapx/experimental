26e716917d3f6f81427be281bd2617376a925cb1
KAPIL <xkapil@gmail.com>
2015-11-26 16:17:10 +0000

    initial version

---
+++ big-data/apache-storm/src/main/java/com/kapx/bigdata/topology/CommitFeedListener.java
@@ -0,0 +1,45 @@
+package com.kapx.bigdata.topology;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
+
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichSpout;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+
+@SuppressWarnings("serial")
+public class CommitFeedListener extends BaseRichSpout {
+	private SpoutOutputCollector outputCollector;
+	private List<String> commits;
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+		outputFieldsDeclarer.declare(new Fields("commit"));
+	}
+
+	@Override
+	@SuppressWarnings("rawtypes")
+	public void open(Map map, TopologyContext context, SpoutOutputCollector outputCollector) {
+		this.outputCollector = outputCollector;
+
+		try {
+			commits = IOUtils.readLines(ClassLoader.getSystemResourceAsStream("changelog.txt"), Charset.defaultCharset().name());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void nextTuple() {
+		for (String commit : commits) {
+			outputCollector.emit(new Values(commit));
+		}
+	}
+}
