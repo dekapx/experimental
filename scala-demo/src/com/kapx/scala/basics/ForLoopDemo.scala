c1ced66f51a9076597347b4179bc45befa531928
KAPIL <xkapil@gmail.com>
2016-07-26 16:13:56 +0100

    code cleanup

--- scala-demo/src/com/kapx/scala/basics/ForLoopDemo.scala
+++
@@ -1,7 +0,0 @@
-package com.kapx.scala.basics
-
-object ForLoopDemo extends App {
-    for(i <- 0 to 9) {
-        println("Number: " + i)
-    }
-}
--- scala-demo/src/com/kapx/scala/basics/HelloWorld.scala
+++
@@ -1,6 +0,0 @@
-package com.kapx.scala.basics
-
-object HelloWorld extends App {
-    val message:String = "DeKapx"
-    println(message)
-}
