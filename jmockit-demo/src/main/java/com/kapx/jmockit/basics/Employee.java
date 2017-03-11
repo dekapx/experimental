707087a533da1a9068f94418e88a09191636f46b
dekapx <dekapx@gmail.com>
2017-03-11 00:17:09 +0000

    code cleanup

--- jmockit-demo/src/main/java/com/kapx/jmockit/basics/Employee.java
+++
@@ -1,9 +0,0 @@
-package com.kapx.jmockit.basics;
-
-public class Employee {
-    private Long id;
-
-    public Long getId() {
-        return id;
-    }
-}
--- jmockit-demo/src/main/java/com/kapx/jmockit/basics/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-package com.kapx.jmockit.basics;
-
-public interface EmployeeService {
-	int getCount();
-
-	Employee save(Employee employee);
-}
