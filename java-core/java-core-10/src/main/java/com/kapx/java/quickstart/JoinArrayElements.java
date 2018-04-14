2b01cac6bcb55ad10e87c772f0c982cf91470574
dekapx <dekapx@gmail.com>
2018-04-14 14:06:04 +0100

    use of function

--- java-core/java-core-10/src/main/java/com/kapx/java/quickstart/JoinArrayElements.java
+++ java-core/java-core-10/src/main/java/com/kapx/java/quickstart/JoinArrayElements.java
@@ -1,18 +1,21 @@
 package com.kapx.java.quickstart;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class JoinArrayElements {
     public static void main(String[] args) {
-        final String[] languages = {"Java", "Scala", "Kotlin"};
-        var output = arrayAsCommaSeperatedString(languages);
+        var languages = List.of("Java", "Scala", "Kotlin");
+        var output = toCommaSeparatedValues.apply(languages);
         System.out.println(output);
     }
 
-    private static String arrayAsCommaSeperatedString(final String[] elements) {
-        return Arrays
-                .stream(elements)
-                .collect(Collectors.joining(","));
-    }
+    /**
+     * Accept List and convert to command separated String
+     */
+    private static Function<List<String>, String> toCommaSeparatedValues = (elements) ->
+            elements.stream()
+                    .collect(Collectors.joining(","));
+
 }
