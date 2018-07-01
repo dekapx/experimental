528cb4ac76a1db7c79ccc979c23e34e33af1ff78
dekapx <dekapx@gmail.com>
2018-07-01 19:57:25 +0100

    server entity repository added

--- spring-boot-docker/src/main/java/com/dekapx/springbootdocker/service/ServerServiceImpl.java
+++ spring-boot-docker/src/main/java/com/dekapx/springbootdocker/service/ServerServiceImpl.java
@@ -1,64 +1,40 @@
 package com.dekapx.springbootdocker.service;
 
+import com.dekapx.springbootdocker.domain.ServerEntity;
 import com.dekapx.springbootdocker.dto.ServerDto;
+import com.dekapx.springbootdocker.repository.ServerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ServerServiceImpl implements ServerService {
+    private ServerRepository serverRepository;
+
+    @Autowired
+    public ServerServiceImpl(final ServerRepository serverRepository) {
+        this.serverRepository = serverRepository;
+    }
+
     @Override
     public ServerDto find(final long id) {
-        return ServerDto.builder()
-                .id(1L)
-                .name("Dell PowerEdge T20")
-                .build();
+        final Optional<ServerEntity> optionalEntity = serverRepository.findById(id);
+        return toServerDto(optionalEntity.orElseThrow(() -> new ServerNotFoundException(String.valueOf(id))));
     }
 
     @Override
     public List<ServerDto> findAll() {
-        final List<ServerDto> dtos = new ArrayList<>();
-        dtos.add(ServerDto.builder()
-                .id(1L)
-                .name("Dell PowerEdge T20")
-                .build());
-        dtos.add(ServerDto.builder()
-                .id(2L)
-                .name("Lenovo ThinkServer TS140")
-                .build());
-        dtos.add(ServerDto.builder()
-                .id(3L)
-                .name("Supermicro SuperWorkstation 5039A-IL")
-                .build());
-        dtos.add(ServerDto.builder()
-                .id(4L)
-                .name("Fujitsu Primergy TX1310 M1")
-                .build());
-        dtos.add(ServerDto.builder()
-                .id(5L)
-                .name("HP Proliant Microserver Gen8")
-                .build());
-        dtos.add(ServerDto.builder()
-                .id(5L)
-                .name("HP Proliant Microserver Gen8")
-                .build());
-        dtos.add(ServerDto.builder()
-                .id(6L)
-                .name("Lenovo ThinkServer TS440")
-                .build());
-        dtos.add(ServerDto.builder()
-                .id(7L)
-                .name("HP Proliant ML350 Gen 9")
-                .build());
-        dtos.add(ServerDto.builder()
-                .id(8L)
-                .name("Scan 3XS SER-T25")
-                .build());
-        dtos.add(ServerDto.builder()
-                .id(9L)
-                .name("Asus TS500")
-                .build());
-        return dtos;
+        return null;
+    }
+
+
+    private ServerDto toServerDto(final ServerEntity entity) {
+        return ServerDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .build();
     }
 }
