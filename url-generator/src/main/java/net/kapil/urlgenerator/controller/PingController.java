f99a0d50c33ff7620d4c2d474efdd2b18347bc0d
dekapx <dekapx@gmail.com>
2018-07-21 22:37:34 +0100

    docker file and pom changes

--- url-generator/src/main/java/net/kapil/urlgenerator/controller/PingController.java
+++ url-generator/src/main/java/net/kapil/urlgenerator/controller/PingController.java
@@ -1,15 +1,12 @@
 package net.kapil.urlgenerator.controller;
 
-        import org.springframework.http.MediaType;
-        import org.springframework.web.bind.annotation.ExceptionHandler;
-        import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.PathVariable;
-        import org.springframework.web.bind.annotation.RequestMapping;
-        import org.springframework.web.bind.annotation.RequestMethod;
-        import org.springframework.web.bind.annotation.ResponseStatus;
-        import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-        import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.CONFLICT;
 
 @RestController
 @RequestMapping("/api")
--- url-generator/src/test/java/net/kapil/urlgenerator/service/UrlServiceIT.java
+++ url-generator/src/test/java/net/kapil/urlgenerator/service/UrlServiceIT.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UrlServiceTest {
+public class UrlServiceIT {
     @Rule
     public ExpectedException expected = ExpectedException.none();
 
