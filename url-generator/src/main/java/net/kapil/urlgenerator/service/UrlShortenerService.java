2f1c37df065f894a545fdaa4de544af02683e273
dekapx <dekapx@gmail.com>
2018-07-24 21:50:22 +0100

    repo clean up

--- url-generator/src/main/java/net/kapil/urlgenerator/service/UrlShortenerService.java
+++
@@ -1,5 +0,0 @@
-package net.kapil.urlgenerator.service;
-
-public interface UrlShortenerService {
-    String getShortUrl(String longUrl);
-}
--- url-generator/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT into URLS (ORIGINAL_URL, SHORTEN_URL)
-VALUES ('http://localhost:9090/api/ping', 'http://tinyurl/dummy')
