fc52b117ca237ba71ab07c2e0fa9ecab9b6a459c
dekapx <dekapx@gmail.com>
2018-08-25 17:19:14 +0100

    consumer andThen example

--- java-core/java-core-10/src/main/java/com/kapx/java/quickstart/Test.java
+++ java-core/java-core-10/src/main/java/com/kapx/java/quickstart/Test.java
@@ -1,17 +1,8 @@
 package com.kapx.java.quickstart;
 
-import java.util.function.Consumer;
-import java.util.stream.Stream;
-
 public class Test {
     public static void main(String[] args) {
-        final Stream<String> languages = Stream.of("Java", "Scala", "Kotlin", "Python", "Closure");
-
-        languages.forEach(toUpperCase.andThen(printLength));
+        
     }
-
-    private static Consumer<String> toUpperCase = (language) -> System.out.print(language.toUpperCase());
-
-    private static Consumer<String> printLength = (language) -> System.out.println(" - " + language.length());
 }
 
